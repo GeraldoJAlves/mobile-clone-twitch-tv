6538b752197a7caf25ccf0c6efaef2cb765c0d3c
Geraldo Alves <junior.alves.2007@gmail.com>
2020-08-31 22:58:17 -0300

    add pages

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
-import StatusBar from '@react-native-community/status-bar';
 import React from 'react';
+import {StatusBar, View, Platform, Text } from 'react-native';
 
 import Routes from './src/routes';
 
 export default function App() {
- 
+
+  const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 40 : 0;
   return (
     <>
+      <View style={{height:STATUSBAR_HEIGHT,backgroundColor:"#000" }} >
+        <StatusBar barStyle="light-content" backgroundColor="#000" />
+      </View>
       <Routes />
-      <StatusBar />
-
     </>
   );
 }
