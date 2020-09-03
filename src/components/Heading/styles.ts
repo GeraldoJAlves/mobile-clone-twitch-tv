d424a8fccb5f4f63f5379711cef28252192f0df3
Geraldo Alves <junior.alves.2007@gmail.com>
2020-09-03 00:14:43 -0300

    add Heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+import colors from '../../styles/color';
+
+export const Container = styled.View`
+  padding: 10px 10px 20px;
+`;
+
+export const HeadingText = styled.Text`
+  color: ${colors.black};
+  font-size: 30px;
+  font-weight: bold;
+`;
